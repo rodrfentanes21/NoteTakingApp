84ada6593fea40193b10a0ff0023f0a771d2895b
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-08-10 16:52:43 -0300

    interface props @ Note and Title

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+interface NoteProps {
+  noteTitle: string;
+}
+
 const NoteComponent = styled.Text`
   font-size: 30px;
   color: dodgerblue;
   padding-left: 30px;
 `;
-const Note = ({noteTitle}) => {
+const Note = ({noteTitle}: NoteProps) => {
   return <NoteComponent>{noteTitle}</NoteComponent>;
 };
 
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+interface TitleProps {
+  title: string;
+}
+
 const TitleText = styled.Text`
   font-size: 50px;
   color: dodgerblue;
@@ -14,7 +18,7 @@ const TitleContainer = styled.View`
   justify-content: center;
 `;
 
-const Title = ({title}) => {
+const Title = ({title}: TitleProps) => {
   return (
     <TitleContainer>
       <TitleText>{title}</TitleText>
