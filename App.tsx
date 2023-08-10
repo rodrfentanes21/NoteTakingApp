6e41cf5eb843f73b9f3753145d5c04e2a8199e03
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-08-10 15:06:33 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Apppppp</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
